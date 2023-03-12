72bf67c5d1d98d9ac518dad186f2ff617bf1d586
ahmedgaboelnaga <ahmedgaboelnaga@gmail.com>
2023-03-12 21:56:10 +0200

    Add project files.

---
+++ Problem1_Q7/FileName.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	float length = 8.6, width = 5.3;
+
+	cout << "Area: " << length * width << endl;
+
+	cout << "Perimeter" << 2 * (length + width) << endl;
+
+	return 0;
+}
