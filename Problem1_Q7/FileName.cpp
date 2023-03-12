530fdf8966dce43328cdb1c933aa6446114dc1c8
ahmedgaboelnaga <ahmedgaboelnaga@gmail.com>
2023-03-12 22:08:21 +0200

    comment

--- Problem1_Q7/FileName.cpp
+++ Problem1_Q7/FileName.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
+	//7.(Area and perimeter of a rectangle) Write a program that displays the area and perimeter of a rectangle with a width of 5.3 and height of 8.6 
+	//using the following formula : area = width * height perimeter = 2 * (width + height)
+
 	float length = 8.6, width = 5.3;
 
 	cout << "Area: " << length * width << endl;
